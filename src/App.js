0830066e4f567454ba669aa837fa9c31e04c70ae
benbasil1 <benbasilreji6@gmail.com>
2024-07-20 00:46:01 +0530

    view created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddMovie from './components/AddMovie';
+import ViewAllMovie from './components/ViewAllMovie';
 
 function App() {
   return (
     <div >
       <AddMovie/>
+      <ViewAllMovie/>
     </div>
   );
 }
---
+++ src/components/ViewAllMovie.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const ViewAllMovie = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+          <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Movie Name</th>
+      <th scope="col">Actor</th>
+      <th scope="col">Actress</th>
+      <th scope="col">Director</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Rajamanikkam</th>
+      <td>shake abdhulla</td>
+      <td>aju</td>
+      <td>Robin Lomin</td>
+    </tr>
+    
+  </tbody>
+</table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ViewAllMovie
