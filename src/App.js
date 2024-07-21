02c640797fceab03a85484cdd2bf2f4866ff5130
benbasil1 <benbasilreji6@gmail.com>
2024-07-21 13:22:27 +0530

    routing completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import AddMovie from './components/AddMovie';
 import ViewAllMovie from './components/ViewAllMovie';
+import Nav from './components/Nav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div >
-      <AddMovie/>
-      <ViewAllMovie/>
+     <Nav/>
+      <BrowserRouter>
+      <Routes>
+
+       
+        <Route path="/" element={<AddMovie/>} />
+        <Route path="/view" element={<ViewAllMovie/>} />
+
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
